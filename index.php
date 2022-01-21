920889d1b46472e3edfee945c26c9cce73cf37b8
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-21 15:57:38 +0100

    Salvo la parola da censurare in una variabile

--- index.php
+++ index.php
@@ -8,12 +8,16 @@
 </head>
 <body>
     <?php
-        $original_text = 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.'
+        $original_text = 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.';
+        $censored_word = $_GET["censored_word"];
     ?>
 
     <h2>Paragrafo originale</h2>
     <p><?php echo $original_text ?></p>
     <span>Lunghezza: <?php echo strlen($original_text) ?></span>
+
+    
+    <?php var_dump($censored_word) ?>
 
 </body>
 </html>
