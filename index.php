dcc391c36cec5e92df55dfc3a179679e8231d5d5
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-21 15:48:18 +0100

    Inserito testo originale e lunghezza

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-test
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    <?php
+        $original_text = 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.'
+    ?>
+
+    <h2>Paragrafo originale</h2>
+    <p><?php echo $original_text ?></p>
+    <span>Lunghezza: <?php echo strlen($original_text) ?></span>
+
+</body>
+</html>
